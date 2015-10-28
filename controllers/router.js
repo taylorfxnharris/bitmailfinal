76415512e66534387ba2b91e5d4aadbec62b1395
Taylor Harris <thharris10@gmail.com>
2015-10-28 15:25:54 -0400

    updating

--- controllers/router.js
+++ controllers/router.js
@@ -1,10 +1,19 @@
 var home = require('./home');
 var users = require('./users');
+var pages = require('./pages');
+var message = require('./message');
 
 // Map routes to controller functions
 module.exports = function(app) {
     // Render home page
-    app.get('/', home.show);
+    app.get('/index2', home.show);
+
+    // Twilio SMS webhook route
+    app.post('/message', message.webhook);
+
+    // Render a page that will allow an administrator to send out a message
+    // to all subscribers
+    app.get('/', pages.showForm);
 
     // Routes for account creation
     app.get('/users/new', users.showCreate);
@@ -13,4 +22,5 @@ module.exports = function(app) {
     app.post('/users/:id/verify', users.verify);
     app.post('/users/:id/resend', users.resend);
     app.get('/users/:id', users.showUser);
+    app.post('/message/send', message.sendMessages);
 };
